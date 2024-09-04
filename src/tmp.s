724c9fe64229e25f88a289c2c3ad12588d5b78f4
decrazyo <decrazyo@gmail.com>
2024-09-03 21:22:22 -0500

    remove unused functions

--- src/tmp.s
+++ src/tmp.s
@@ -1,20 +1,29 @@
 
+; this module provides temporary memory for any code that needs it.
+; care must be taken when using these variables since any code can change them.
+
+; use of these variables inside of interrupt handlers is allowed.
+; interrupt handlers must save the content of a temp variable before using it
+; and restore the content before returning from the interrupt handler.
+; for that reason, the use of this temporary memory in interrupt handler is discouraged.
+
+; the following general rules are followed to prevent conflicts in this memory space.
+; when using these variables to pass arguments between functions,
+; lower numbered variables should be used first.
+; when using these variables to temporarily store data within a function,
+; higher numbered variables should be used first.
+
 .include "tmp.inc"
 
-.exportzp zd0
-.exportzp zw0
-.exportzp zb0
-.exportzp zb1
-.exportzp zw1
-.exportzp zb2
-.exportzp zb3
-
-.export set_zp_ptr0
-.export set_zp_ptr1
-.export set_ptr0
-.export set_ptr1
-
-.export memcpy
+; many variables refer to the same memory locations.
+; the different names are intended to clarify usage elsewhere in code.
+.exportzp zd0 ; 32-bit variable (uses zb0 - zb3)
+.exportzp zw0 ; 16-bit variable (uses zb0 - zb1)
+.exportzp zb0 ; 8-bit variable
+.exportzp zb1 ; 8-bit variable
+.exportzp zw1 ; 16-bit variable (uses zb2 - zb3)
+.exportzp zb2 ; 8-bit variable
+.exportzp zb3 ; 8-bit variable
 
 .segment "TEMP":zp
 
@@ -28,64 +37,3 @@ zb3: .res 1
 
 ; some functions may depend on this fact.
 .assert zb0 = 0, error, "temporary memory does not start at address 0"
-
-.segment "LOWCODE"
-
-; copy a zero-page pointer into the 0th temp word.
-; < A = address low byte
-; changes: X
-.proc set_zp_ptr0
-    ldx #0
-    beq set_ptr0 ; branch always
-    ; [tail_branch]
-.endproc
-
-; copy a zero-page pointer into the 1st temp word.
-; < A = address low byte
-; changes: X
-.proc set_zp_ptr1
-    ldx #0
-    beq set_ptr1 ; branch always
-    ; [tail_branch]
-.endproc
-
-; copy a pointer into the 0th temp word.
-; < A = address low byte
-; < X = address high byte
-.proc set_ptr0
-    sta zw0
-    stx zw0+1
-    rts
-.endproc
-
-
-; copy a pointer into the 1st temp word.
-; < A = address low byte
-; < X = address high byte
-.proc set_ptr1
-    sta zw1
-    stx zw1+1
-    rts
-.endproc
-
-
-; copy Y bytes from the 0th temp pointer to the 1st temp pointer.
-; < zw0 = source pointer
-; < zw1 = destination pointer
-; < Y = number of bytes to copy
-; changes: A, Y
-.proc memcpy
-    dey
-loop:
-    lda (zw0), y
-    sta (zw1), y
-    dey
-    bpl loop
-    rts
-.endproc
-
-
-.proc memset
-    ; TODO: implement memset
-    ;       might be useful for zeroing out registers
-.endproc
